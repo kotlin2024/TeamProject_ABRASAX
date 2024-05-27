11f7a5cbdfc7e3809d43059e02fec892d727d5b8
TONIXX <jiseok95@gmail.com>
2024-05-27 15:37:30 +0900

    init: project

---
+++ src/main/kotlin/com/teamsparta/abrasax/AbrasaxApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.abrasax
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AbrasaxApplication
+
+fun main(args: Array<String>) {
+    runApplication<AbrasaxApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/abrasax/AbrasaxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.abrasax
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AbrasaxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
